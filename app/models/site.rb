7148190ef98d37e5f594a67b68f7588f037ca3a0
stephane lam <lamstephane@hotmail.com>
2019-01-11 00:13:31 +0100

    formuliare

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,6 @@
 class Site < ApplicationRecord
   self.table_name = 'sites'
+  validates_uniqueness_of(:reference_site)
 	has_many :parts, :inverse_of => :contrat, :class_name => "Part", :foreign_key => "reference_site", :dependent => :destroy
 
 end
