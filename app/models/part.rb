efef307bf501734720ea2bf93645e5a2fbc6466e
stephane lam <lamstephane@hotmail.com>
2019-01-10 20:25:58 +0100

    creation tables

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,6 @@
 class Part < ApplicationRecord
+    validates_uniqueness_of(:reference_part)
+
+    
+	belongs_to :site, :inverse_of => :propositions, :class_name => "Site", :foreign_key => "reference_site"
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,4 @@
 class Site < ApplicationRecord
+    self.table_name = 'sites'
+	has_many :parts, :inverse_of => :contrat, :class_name => "Part", :foreign_key => "reference_site", :dependent => :destroy
 end
