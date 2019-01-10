e87d3cadf102f5b6c777d0562a0422457b20c021
stephane lam <lamstephane@hotmail.com>
2019-01-10 23:23:43 +0100

    creation vue site

--- app/models/part.rb
+++ app/models/part.rb
@@ -3,6 +3,5 @@ class Part < ApplicationRecord
 	self.table_name = 'parts'
     validates_uniqueness_of(:reference_part)
 
-
 	belongs_to :site, :inverse_of => :propositions, :class_name => "Site", :foreign_key => "reference_site"
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,5 @@
 class Site < ApplicationRecord
-    self.table_name = 'sites'
+  self.table_name = 'sites'
 	has_many :parts, :inverse_of => :contrat, :class_name => "Part", :foreign_key => "reference_site", :dependent => :destroy
+
 end
