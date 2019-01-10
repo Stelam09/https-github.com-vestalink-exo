37bf1acf5263933e9a778b7a0e32e711b07c31be
stephane lam <lamstephane@hotmail.com>
2019-01-10 20:44:13 +0100

    creation tables

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,6 +1,8 @@
 class Part < ApplicationRecord
+
+	self.table_name = 'parts'
     validates_uniqueness_of(:reference_part)
 
-    
+
 	belongs_to :site, :inverse_of => :propositions, :class_name => "Site", :foreign_key => "reference_site"
 end
