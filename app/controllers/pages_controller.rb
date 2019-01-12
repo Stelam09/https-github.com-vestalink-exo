3154a865dd180408d61eadfa90139f4418e5d893
stephane lam <lamstephane@hotmail.com>
2019-01-12 23:12:58 +0100

    vues

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,8 @@
 
 class PagesController < ApplicationController
 	def import
-		
-		rails import:site
-  rails import:part
-  redirect_to root_url, notice "Data Imported !"
+  	rails import_sites_csv:site
+  	rails import_sites_csv:part
+  	redirect_to root_url, notice "Data Imported !"
 	end
 end
