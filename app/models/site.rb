b081fe4a8aa9c0c3f5fde489bbbb18837963a2b7
stephane lam <lamstephane@hotmail.com>
2019-01-13 14:04:24 +0100

    vues

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,6 +3,15 @@
 class Site < ApplicationRecord
   self.table_name = 'sites'
   validates_uniqueness_of(:reference_site)
-	has_many :parts, :inverse_of => :contrat, :class_name => "Part", :foreign_key => "reference_site", :dependent => :destroy
+  has_many :parts, :inverse_of => :contrat, :class_name => "Part", :foreign_key => "reference_site", :dependent => :destroy
+  
 
+  def self.return_adress_link_google_maps(adress)
+  	require 'geokit'
+    include GeoKit::Geocoders
+
+    coords = MultiGeocoder.geocode(adress)
+
+    link_google_maps = "http://maps.google.co.uk/maps?q=#{coords.lat},#{coords.lng}"
+  end
 end
