f031ad67169861e3a57fb60ef1ffe9e73a0ce620
root <root@debian.debian>
2019-01-14 00:32:31 +0100

    last commit

--- app/models/part.rb
+++ app/models/part.rb
@@ -3,7 +3,9 @@
 class Part < ApplicationRecord
 
 	self.table_name = 'parts'
-    validates_uniqueness_of(:reference_part)
+
+  validates_uniqueness_of(:reference_part)
 
 	belongs_to :reference_site, :inverse_of => :parts, :class_name => "Site", :foreign_key => "reference_site_id"
+
 end
