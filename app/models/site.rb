0f5a37a0809d4c4e8fdcfecc453610e593736feb
root <root@debian.debian>
2019-01-14 14:19:32 +0100

    last commit

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,10 +6,4 @@ class Site < ApplicationRecord
   validates_uniqueness_of(:reference_site)
   has_many :parts, :inverse_of => :reference_site, :class_name => "Part", :foreign_key => "reference_site_id", :dependent => :destroy
   
-
-  def self.return_adress_link_google_maps(adress)
-    coords = MultiGeocoder.geocode(adress)
-
-    "http://maps.google.co.uk/maps?q=#{coords.lat},#{coords.lng}"
-  end
 end
